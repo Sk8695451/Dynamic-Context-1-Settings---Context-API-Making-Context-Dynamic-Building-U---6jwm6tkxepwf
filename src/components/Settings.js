df1a3fd102920af13e817b7366e50767a7a600bc
unknown <sk8695451>
2022-12-19 02:05:38 +0530

    dd

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import { useContext } from "react"
 import { UserContext } from "../context/userContext"
 
 export const Settings = () => {
+    const [val, setVal] = useState("");
+    const [greeting, setGreeting] = useContext(UserContext);
 
-    // to be implemented in context
-    const { changeGreeting } = useContext(UserContext)
+    const handlechange = (event) => {
+        setVal(event.target.value);
+
+        setGreeting(event.target.value);
+
+
+    }
 
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} />
+            <input type={'text'} value={val} onChange={handlechange} />
         </div>
     )
 }
